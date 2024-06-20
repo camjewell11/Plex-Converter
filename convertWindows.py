c22f70b6ffa226f0b79a0e14e800289f04dc5c30
camjewell11 <camjewell50@gmail.com>
2024-06-20 17:04:22 -0500

    Updated windows

--- convertWindows.py
+++ convertWindows.py
@@ -1,8 +1,12 @@
-import os, subprocess, json
+import os, subprocess, json, logging
 from tkinter import filedialog, Tk
 
 lastDirectoryPath = 'last_dir.json'
 lastHandbrakecliPath = 'last_handbrakecli.json'
+
+# Set up logging
+logging.basicConfig(filename='conversion.log', level=logging.INFO,
+                    format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
 def convert_files(directory, extension=".mkv"):
     handbrakecli_path = get_handbrakecli_path()
@@ -13,16 +17,24 @@ def convert_files(directory, extension=".mkv"):
                 mp4_file = os.path.join(dirpath, os.path.splitext(filename)[0] + ".mp4").replace("\\", "/")
                 command = [handbrakecli_path, "-i", mkv_file, "-o", mp4_file]
                 print(f"\nStarting conversion of {filename}")
-                process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-                for line in iter(process.stdout.readline, b''):
-                    line = line.decode()
-                    if "Encoding: " in line:
-                        print(line, end='')
-                print(f"\nFinished conversion of {filename}")
+                logging.info(f"Starting conversion of {filename}")
+                try:
+                    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                    for line in iter(process.stdout.readline, b''):
+                        line = line.decode()
+                        if "Encoding: " in line:
+                            print(line, end='')
+                except Exception as e:
+                    print(f"An error occurred while converting {filename}: {e}")
+                    logging.error(f"An error occurred while converting {filename}: {e}")
+                else:
+                    print(f"\nFinished conversion of {filename}")
+                    logging.info(f"Finished conversion of {filename}")
+                    if os.path.exists(mp4_file):  # Check if the MP4 file was created
+                        os.remove(mkv_file)  # Delete the MKV file
 
 def get_handbrakecli_path():
-    # os.path.join(os.getcwd(), "HandBrakeCLI.exe").replace("\\", "/")
-    handbrakecli_path = None
+    handbrakecli_path = os.path.join(os.getcwd(), "HandBrakeCLI.exe").replace("\\", "/")
     try:
         with open(lastHandbrakecliPath, 'r') as f:
             handbrakecli_path = json.load(f)
